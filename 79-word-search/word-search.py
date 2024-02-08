8afd2647ad4f32de5df7c43ceecd21a54b4154a5
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-02-08 09:44:51 +0530

    Time: 4058 ms (66.25%) | Memory: 16.6 MB (75.98%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,22 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        row , col = len(board),len(board[0])
+        row = len(board)
+        col = len(board[0])
         visit = set()
-        
-        def dfs(r,c,i):
-            if i==len(word):
+        def backtracking(r,c,i):
+            if i == len(word):
                 return True
-            if (r<0 or c<0 or r >= row or c >= col or word[i] != board[r][c] or (r,c) in visit):
+            if r>=row or c>=col or ((r,c) in visit) or word[i]!=board[r][c] or r<0 or c<0:
                 return False
             visit.add((r,c))
-            res = (dfs(r+1,c,i+1) or
-                    dfs(r-1,c,i+1) or
-                    dfs(r,c+1,i+1) or
-                    dfs(r,c-1,i+1))
+            res = (backtracking(r+1,c,i+1) or
+            backtracking(r-1,c,i+1) or
+            backtracking(r,c+1,i+1) or
+            backtracking(r,c-1,i+1) )
             visit.remove((r,c))
             return res
-        for i in range(row):
-            for j in range(col):
-                if dfs(i,j,0):
+        for r in range(row):
+            for c in range(col):
+                if backtracking(r,c,0):
                     return True
         return False
+        
