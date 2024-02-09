60673ed2c832b293c5a8f5aa12c8704bff345fa9
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-02-09 11:52:57 +0530

    Time: 573 ms (18.74%) | Memory: 40.3 MB (8.05%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,12 +1,11 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        n=len(s)
         res = []
+        n=len(s)
         if n==0:
             return [[]]
         for i in range(1,n+1):
-            if s[:i]!=s[:i][::-1]:
-                continue
-            for suf in self.partition(s[i:]):
-                res.append([s[:i]]+suf)
+            if s[:i]==s[:i][::-1]:
+                for suf in self.partition(s[i:]):
+                    res.append([s[:i]]+suf)
         return res
