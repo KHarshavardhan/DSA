033f9d513c4c932a5064c563b96775d87d081575
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-02-09 12:23:09 +0530

    Time: 47 ms (87.20%) | Memory: 17 MB (61.87%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,27 +1,27 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        res = []
+        res=[]
         col = set()
-        PostDiag = set() #(r-c)
-        NegDiag = set() #(r+c)
-
-        board = [["."]*n for i in range(n)]
+        posDiag = set() #(r-c)
+        negDiag = set() #(r+c)
+        board = [['.']*n for i in range(n)]
         def backtracking(r):
-            if r == n:
+            if r==n:
                 temp = ["".join(row) for row in board]
                 res.append(temp)
                 return
             for c in range(n):
-                if c in col or (r-c) in PostDiag or (r+c) in NegDiag :
-                    continue
+                if c in col or (r+c) in negDiag or (r-c) in posDiag:
+                    continue 
                 col.add(c)
-                PostDiag.add(r-c)
-                NegDiag.add(r+c)
-                board[r][c] = "Q"
+                negDiag.add(r+c)
+                posDiag.add(r-c)
+                board[r][c]="Q"
                 backtracking(r+1)
                 col.remove(c)
-                PostDiag.remove(r-c)
-                NegDiag.remove(r+c)
-                board[r][c] = "."
+                negDiag.remove(r+c)
+                posDiag.remove(r-c)
+                board[r][c]="."
         backtracking(0)
         return res
+                
