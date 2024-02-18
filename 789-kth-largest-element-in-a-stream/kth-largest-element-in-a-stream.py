ad7e6ea7ee933bfe48b5559a0d5848400a6e0cd5
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-02-18 11:56:35 +0530

    Time: 77 ms (76.83%) | Memory: 20.4 MB (83.54%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,22 +1,16 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.minHeap,self.k = nums,k
-        #create min heap using below method
+        self.minHeap = nums
+        self.k=k
         heapq.heapify(self.minHeap)
-        #remove all the min elements from heap to maintain the kth large element
         while len(self.minHeap)>self.k:
             heapq.heappop(self.minHeap)
-
     def add(self, val: int) -> int:
-        #add new element to heap
         heapq.heappush(self.minHeap,val)
-        #if the len is more then k, then remove the min element
-        if len(self.minHeap)>self.k:
+        while len(self.minHeap)>self.k:
             heapq.heappop(self.minHeap)
-        #0th index of heap always stores the kth largest element of heap
         return self.minHeap[0]
-        
 
 
 # Your KthLargest object will be instantiated and called as such:
