e517ab331a7b3de7c53c206b4c595e25b13281d8
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-02-18 12:20:41 +0530

    Time: 663 ms (24.14%) | Memory: 23.3 MB (50.10%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,16 +1,14 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        arr = []
+        minHeap = []
         res = []
-        #to find the distance between a,b from origin 0,0 can be done using a2 + b2 = c2 formula
-        for a,b in points:
-            dist = (a**2)+(b**2)
-            dist = dist**(0.5)
-            arr.append([dist,a,b])
-        #created arr and now create min heap with key as dist , which automatically sorts on dist
-        heapq.heapify(arr)
+        for x,y in points:
+            dist = x**2 + y**2
+            minHeap.append([dist,x,y])
+        heapq.heapify(minHeap)
+        print(minHeap)
         while k>0:
-            dist,a,b = heapq.heappop(arr)
-            res.append([a,b])
+            dist,x,y = heapq.heappop(minHeap)
+            res.append([x,y])
             k-=1
         return res
