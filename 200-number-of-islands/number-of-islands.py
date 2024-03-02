b99d86dadfa14689819f97976d9170a0876ed42e
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-02 12:43:16 +0530

    Time: 261 ms (42.53%) | Memory: 27.9 MB (8.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        row, col = len(grid),len(grid[0])
-        visit = set()
-        noIsland= 0
-        #breath for search is not recursive, its iterative and goes through all possiblilities and adds them to visit set
-        def bfs(r,c):
-            q=deque()
-            q.append((r,c))
+        row,col=len(grid),len(grid[0])
+        visit=set()
+        count=0
+        def dfs(r,c):
+            if (r<0 or c<0 or r>=row or c>=col or (r,c) in visit or grid[r][c]=='0'):
+                return False
             visit.add((r,c))
-
-            while q:
-                r,c=q.popleft()
-                direct = [(1,0),(-1,0),(0,1),(0,-1)]
-                for dr,dc in direct:
-                    if ((r+dr) in range(row) and (c+dc) in range(col) and grid[r+dr][c+dc] == '1' and (r+dr,c+dc) not in visit):
-                        visit.add((r+dr,c+dc))
-                        q.append((r+dr,c+dc))
-        #once deque is empty, that means one island is completed
-        #now entire collection of 1s is considered as one island and moves forward
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            return True
         for r in range(row):
             for c in range(col):
                 if grid[r][c]=='1' and (r,c) not in visit:
-                    bfs(r,c)
-                    noIsland+=1
-        return noIsland
+                    dfs(r,c)
+                    count+=1
+        return count
