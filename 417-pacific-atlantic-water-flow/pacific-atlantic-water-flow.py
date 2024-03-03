9c5d6aa894a3eccf38fc7d306772b7f0bc60033b
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-03 13:30:08 +0530

    Time: 676 ms (11.48%) | Memory: 18.4 MB (44.80%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,28 +1,32 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         row,col=len(heights),len(heights[0])
-        p,a=set(),set()
-
-        def dfs(r,c,visit,prev):
-            if ((r,c) in visit or r<0 or c<0 or r==row or c==col or heights[r][c]<prev):
+        visitp =[]
+        visita=[]
+        res=[]
+        def dfs(r,c,visit,temp):
+            if(r<0 or c<0 or r>=row or c>=col or [r,c] in visit or heights[r][c]<temp):
                 return
-            visit.add((r,c))
-            dfs(r+1,c,visit,heights[r][c])
-            dfs(r-1,c,visit,heights[r][c])
-            dfs(r,c+1,visit,heights[r][c])
-            dfs(r,c-1,visit,heights[r][c])
-
-
-        for c in range(col):
-            dfs(0,c,p,heights[0][c])
-            dfs(row-1,c,a,heights[row-1][c])
-        
-        for r in range(row):
-            dfs(r,0,p,heights[r][0])
-            dfs(r,col-1,a,heights[r][c])
-        res= []
-        for r in range(row):
-            for c in range(col):
-                if (r,c) in p and (r,c) in a:
-                    res.append([r,c])
+            temp=heights[r][c]
+            visit.append([r,c])
+            dfs(r+1,c,visit,temp)
+            dfs(r-1,c,visit,temp)
+            dfs(r,c+1,visit,temp)
+            dfs(r,c-1,visit,temp)
+        for i in range(row):
+            if i==0:
+                for j in range(col):
+                    dfs(i,j,visitp,heights[i][j])
+            else:
+                dfs(i,0,visitp,heights[i][0])
+        for k in range(col):
+            if k==col-1:
+                for j in range(row):
+                    dfs(j,k,visita,heights[j][k])
+            else:
+                dfs(row-1,k,visita,heights[row-1][k])
+        for rp,cp in visitp:
+            if [rp,cp] in visita:
+                res.append([rp,cp])
         return res
+        
