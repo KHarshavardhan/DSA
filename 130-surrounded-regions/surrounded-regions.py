323d42abe1e1d24da123746eff2830150191bc24
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-04 06:47:41 +0530

    Time: 131 ms (26.47%) | Memory: 18.1 MB (55.58%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,29 +3,29 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        row=len(board)
-        col=len(board[0])
-        visit=set()
+        row,col=len(board),len(board[0])
+        visit = set()
 
         def dfs(r,c):
-            if r<0 or c<0 or r==row or c==col or board[r][c]!='O' or (r,c) in visit:
-                return
-            board[r][c]='T'
+            if(r<0 or c<0 or r>=row or c>=col or board[r][c]!='O' or (r,c) in visit):
+                return 
+            visit.add((r,c))
+            board[r][c]='B'
             dfs(r+1,c)
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
+        
         for r in range(row):
             for c in range(col):
-                if (board[r][c]=='O' and (r in [0,row-1] or c in [0,col-1])):
+                if r in [0,row-1] and board[r][c]=='O':
+                    dfs(r,c)
+                elif r in range(1,row-1) and c in [0,col-1] and board[r][c]=='O':
                     dfs(r,c)
         for r in range(row):
             for c in range(col):
-                if (board[r][c]=='O'):
+                if board[r][c]=='O':
                     board[r][c]='X'
-                if (board[r][c]=='T'):
+                if board[r][c]=='B':
                     board[r][c]='O'
-
-        
-        
         
