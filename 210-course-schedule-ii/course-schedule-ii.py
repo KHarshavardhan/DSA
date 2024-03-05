ac72a8bf75bb0b786a531951004b9688e5beae8a
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-05 07:19:54 +0530

    Time: 115 ms (10.63%) | Memory: 18.4 MB (59.16%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,28 +1,27 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        pre = {i:[] for i in range(numCourses)}
-        visit,cycle=set(),set()
-        arr=[]
-        #adding all courses into hashmap
-        for c in prerequisites:
-            pre[c[0]].append(c[1])
-        #dfs will take course value and checks,wheather it is creating a loop, if yes then return false else adds to the visit set, also a hashmap key can have mutiple course as prereq, so it goes through all.
+        pre={i:[] for i in range(numCourses)}
+        visit=set()
+        res=[]
+        for c,p in prerequisites:
+            pre[c].append(p)
         def dfs(crs):
             if crs in visit:
                 return False
-            if crs in cycle:
+            if pre[crs]==[]:
+                if crs not in res:
+                    res.append(crs)
                 return True
             visit.add(crs)
             for p in pre[crs]:
                 if not dfs(p):
                     return False
+            if crs not in res:
+                    res.append(crs)
             visit.remove(crs)
-            cycle.add(crs)
-            arr.append(crs)
             pre[crs]=[]
             return True
-        #last stage to check all indexes and run dfs on all, if nothing return false, i.e it can be completed, hence true is returned
         for x in range(numCourses):
             if not dfs(x):
-                return []      
-        return arr
+                return []
+        return res
