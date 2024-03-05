3eafe761e21879af770fb64d8d982561b60486d9
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-05 07:06:02 +0530

    Time: 78 ms (97.79%) | Memory: 18.3 MB (64.91%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,12 +1,10 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #pre is a hashmap to store cources based on index,here creating a empty hashmap
         pre = {i:[] for i in range(numCourses)}
         visit=set()
-        #adding all courses into hashmap
         for c in prerequisites:
             pre[c[0]].append(c[1])
-        #dfs will take course value and checks,wheather it is creating a loop, if yes then return false else adds to the visit set, also a hashmap key can have mutiple course as prereq, so it goes through all.
+        
         def dfs(crs):
             if crs in visit:
                 return False
@@ -19,7 +17,6 @@ class Solution:
             visit.remove(crs)
             pre[crs]=[]
             return True
-        #last stage to check all indexes and run dfs on all, if nothing return false, i.e it can be completed, hence true is returned
         for x in range(numCourses):
             if not dfs(x):
                 return False
