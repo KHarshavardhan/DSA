bc107332565f1773241ae926e66d7f8d46074a1f
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-09 13:29:18 +0530

    Time: 30 ms (90.56%) | Memory: 16.5 MB (91.07%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,9 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        #[r1,r2,r3,...] this will nums list
         r1,r2=0,0
-
+        #go through all nums and take max of r1+r3,r2
         for r3 in nums:
             c = max(r1+r3,r2)
             r1=r2
             r2=c
+        #finally r2 which is final element will contain the max 
         return r2
