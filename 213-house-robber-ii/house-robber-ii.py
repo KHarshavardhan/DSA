c49bf668a319e3ee54d59192d5ae1730ef3d7325
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-10 12:20:43 +0530

    Time: 34 ms (74.66%) | Memory: 16.5 MB (83.32%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        else:
+            r1,r2,ans1,ans2=0,0,0,0
+            num=nums[:-1]
+            for n in num:
+                temp=max(r1+n,r2)
+                r1=r2
+                r2=temp
+            ans1=r2
+            r1,r2=0,0
+            num=nums[1:]
+            for n in num:
+                temp=max(r1+n,r2)
+                r1=r2
+                r2=temp
+            ans2=r2
+            return max(ans1,ans2)
