237d4aacb1c70e8bfddbc2c6190a11095c6c3323
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-20 11:51:08 +0530

    Time: 55 ms (57.12%) | Memory: 16.6 MB (82.38%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,11 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        #append 0 to the end of cost to set destination
-        cost.append(0)
-        #from last 3rd element, check the min of next two elements and add to current.
+        res=0
         for i in range(len(cost)-3,-1,-1):
             cost[i]=cost[i]+min(cost[i+1],cost[i+2])
-        #remove added extra 0
-        cost.pop(len(cost)-1)
-        #result will be minimum of first and second element
         return min(cost[0],cost[1])
