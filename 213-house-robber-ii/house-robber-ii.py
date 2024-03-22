6f2fa4b3d1983f45aba2f4c69f8aa9ac41f91c08
Harsha Vardhan Kurakula <77850731+KHarshavardhan@users.noreply.github.com>
2024-03-22 15:20:37 +0530

    Time: 38 ms (46.40%) | Memory: 16.6 MB (12.13%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #need to run the house robber 1 sol twice for two subsets
         if len(nums)==1:
             return nums[0]
-        else:
-            r1,r2,ans1,ans2=0,0,0,0
-            num=nums[:-1]
-            for n in num:
-                temp=max(r1+n,r2)
-                r1=r2
-                r2=temp
-            ans1=r2
-            r1,r2=0,0
-            num=nums[1:]
-            for n in num:
-                temp=max(r1+n,r2)
-                r1=r2
-                r2=temp
-            ans2=r2
-            return max(ans1,ans2)
+        r1,r2=0,0
+        ans1,ans2=0,0
+        for r in nums[:-1]:
+            temp=max(r1+r,r2)
+            r1=r2
+            r2=temp
+        ans1=r2
+        r1,r2=0,0
+        for r in nums[1:]:
+            temp=max(r1+r,r2)
+            r1=r2
+            r2=temp
+        ans2=r2
+        return max(ans1,ans2)
